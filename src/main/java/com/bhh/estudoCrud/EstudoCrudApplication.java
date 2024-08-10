ea38151b4bc01999cf53eee3958db54b9e138096
Bruno H. Homem <brunohhomem@gmail.com>
2024-08-10 09:54:33 -0300

    first commit

---
+++ src/main/java/com/bhh/estudoCrud/EstudoCrudApplication.java
@@ -0,0 +1,13 @@
+package com.bhh.estudoCrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudoCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudoCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bhh/estudoCrud/EstudoCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhh.estudoCrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudoCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
