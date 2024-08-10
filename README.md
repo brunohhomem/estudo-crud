00316660e11d8da87a492ace2c58e4521f1f3c40
Bruno H. Homem <brunohhomem@gmail.com>
2024-08-10 10:12:46 -0300

    chore: configuring database and dependencies

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# estudo-crud
+# Projeto de Estudo Crud
+
+Tecnologias utilizadas
+- Spring Boot 3.3.2
+- Java 17
+- PostgreSQL
+
+Dependências do projeto
+- JPA
+- Lombok
+- PostgreSQL Conector
+- Devtools
+- Spring Web
--- src/main/java/com/bhh/estudoCrud/Application.java
+++ src/main/java/com/bhh/estudoCrud/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EstudoCrudApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EstudoCrudApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
