e0086a3a7bfddbdad96a10574b10007ea1b1b600
Bruno H. Homem <brunohhomem@gmail.com>
2024-08-17 19:11:54 -0300

    feat: added Produto Entity

---
+++ src/main/java/com/bhh/estudoCrud/entity/Produto.java
@@ -0,0 +1,23 @@
+package com.bhh.estudoCrud.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private Boolean isActive;
+}
